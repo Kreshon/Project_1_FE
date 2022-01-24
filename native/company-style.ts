8041c1e6495afdf71114af336030a8588538d917
Nick Kreshon <nickkree@gmail.com>
2022-01-23 23:42:49 -0500

    adjusting folders

---
+++ native/company-style.ts
@@ -0,0 +1,107 @@
+import { StyleSheet } from 'react-native'
+const styles = StyleSheet.create({
+
+input:{
+    //position: "relative",
+    // backgroundColor: "azure",
+    // fontFamily: "Arial, Helvetica, sansSerif",
+    // borderWidth: 5,
+    // borderRadius: 5,
+    // borderStyle: "hidden",
+    // lineHeight: 20,
+},
+
+h1:{
+    //position: "relative",
+    //color: "azure",
+    // fontSize: "xxLarge",
+    // fontFamily: "Arial, Helvetica, sansSerif",
+    //textShadow: 3 "black",
+},
+
+label:{
+    //position: "relative",
+    //color: "azure",
+    //fontSize: "xLarge",
+    //textShadow:2 2 "black",
+},
+
+button:{
+    //position: "relative",
+    //boxShadow: 2 2 "black",
+    fontWeight: "bold",
+    //color: "black",
+    backgroundColor: "azure",
+    borderRadius: 5,
+},
+
+// button:hover{
+//     background-color: rgb(2, 2, 2),
+//     color: azure,
+//     border-radius: 5px,
+// },
+
+// body{
+//     backgroundColor: "grey",
+// },
+
+loginDiv:{
+    position: "absolute",
+    width: 400,
+    height: 200,
+    zIndex: 15,
+    top: "50%",
+    left: "50%",
+    margin: "-100 0 0 -200",
+    textAlign: "center",
+},
+
+addDiv:{
+    position: "absolute",
+    width: 400,
+    height: 200,
+    zIndex: 15,
+    top: "50%",
+    left: "50%",
+    margin: "-100 0 0 -200",
+},
+
+detailLink:{
+    color: "azure",
+    fontSize: 32,
+},
+
+// detailLink:hover{
+//     color: black,
+// },
+
+table:{
+    //border: 2 "black",
+    color: "azure",
+    borderRadius: 5,
+},
+
+tHead:{
+    textAlign: "center",
+},
+
+tData:{
+    textAlign: "center",
+},
+
+p:{
+    position: "relative",
+    color: "azure",
+    fontSize: "x-large",
+    //textShadow: 2 2 "black",
+},
+
+nav:{
+    display: "flex",
+    justifyContent: "space-around",
+    textAlign: "center",
+},
+
+})
+
+export default styles
